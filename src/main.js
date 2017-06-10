d88e9b31201041da099077c547f63a0feb76445c
Jimmy Chandra <cjchandra@hotmail.com>
2017-06-10 18:56:22 +1000

    removed the js

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("angular");
-var app_module_1 = require("./app/app.module");
-angular.bootstrap(document.body, [app_module_1.AppModule.moduleName]);
-//# sourceMappingURL=main.js.map
