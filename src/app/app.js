3888c12e30c9604d16c80b36c0a1376debc56c7b
Jimmy Chandra <cjchandra@hotmail.com>
2017-06-10 17:26:55 +1000

    moves js into its own file

---
+++ src/app/app.js
@@ -0,0 +1,51 @@
+(function() {
+    "use strict";
+
+    angular.module("ajs.hello.world", [])
+        .service("DataService", ["$log", "$q",
+            function ($log, $q) {
+                this.$log = $log;
+                this.$q = $q;
+
+                return {
+                    getData: function getData() {
+                        this.$log.info("[DataService] getData");
+                        return this.$q.resolve("Stranger");
+                    }.bind(this)
+                };
+            }])
+        .component("appRoot", {
+            controller: ["$log", "DataService",
+                function ($log, dataService) {
+                    this.$log = $log;
+                    this.dataService = dataService;
+
+                    this.$onInit = function $onInit() {
+                        this.$log.info("[AppComponent] onInit");
+                        this.greetTarget = "World";
+                    }.bind(this);
+
+                    this.loadData = function loadData() {
+                        this.$log.info("[AppComponent] loadData");
+                        this.dataService
+                            .getData()
+                            .then(function bindData(newGreetTarget) {
+                                this.$log.info("[AppComponent] bindData", newGreetTarget);
+                                this.greetTarget = newGreetTarget;
+                            }.bind(this))
+                            .catch(function handleError(err) {
+                                this.$log.info("[AppComponent] handleError", err);
+                                alert(err);
+                            }.bind(this));
+                    }
+                }],
+            template: [
+                "<div>",
+                "    Hello, {{$ctrl.greetTarget}}!<br>",
+                "    <button type=\"button\" ng-click=\"$ctrl.loadData()\">Load Data...</button>",
+                "</div>"
+            ].join("")
+        });
+
+    angular.bootstrap(document.body, ["ajs.hello.world"]);
+}());
