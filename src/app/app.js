cd72037e2d4ed27fe3d7ec4551e2910d8fb88c85
Jimmy Chandra <cjchandra@hotmail.com>
2017-06-10 18:06:58 +1000

    refactored to JP style guide

--- src/app/app.js
+++ src/app/app.js
@@ -1,51 +1,88 @@
+// app.module
 (function() {
     "use strict";
 
-    angular.module("ajs.hello.world", [])
-        .service("DataService", ["$log", "$q",
-            function ($log, $q) {
-                this.$log = $log;
-                this.$q = $q;
+    angular.module("ajs.hello.world", []);
+}());
 
-                return {
-                    getData: function getData() {
-                        this.$log.info("[DataService] getData");
-                        return this.$q.resolve("Stranger");
-                    }.bind(this)
-                };
-            }])
-        .component("appRoot", {
-            controller: ["$log", "DataService",
-                function ($log, dataService) {
-                    this.$log = $log;
-                    this.dataService = dataService;
+// data.service
+(function() {
+    "use strict";
 
-                    this.$onInit = function $onInit() {
-                        this.$log.info("[AppComponent] onInit");
-                        this.greetTarget = "World";
-                    }.bind(this);
+    DataService.$inject = ["$log", "$q"];
+    function DataService($log, $q) {
+        this.$log = $log;
+        this.$q = $q;
 
-                    this.loadData = function loadData() {
-                        this.$log.info("[AppComponent] loadData");
-                        this.dataService
-                            .getData()
-                            .then(function bindData(newGreetTarget) {
-                                this.$log.info("[AppComponent] bindData", newGreetTarget);
-                                this.greetTarget = newGreetTarget;
-                            }.bind(this))
-                            .catch(function handleError(err) {
-                                this.$log.info("[AppComponent] handleError", err);
-                                alert(err);
-                            }.bind(this));
-                    }
-                }],
-            template: [
-                "<div>",
-                "    Hello, {{$ctrl.greetTarget}}!<br>",
-                "    <button type=\"button\" ng-click=\"$ctrl.loadData()\">Load Data...</button>",
-                "</div>"
-            ].join("")
-        });
+        return {
+            getData: getData.bind(this)
+        };
+
+        function getData() {
+            this.$log.info("[DataService] getData");
+            return this.$q.resolve("Stranger");
+        }
+    }
+
+    angular
+        .module("ajs.hello.world")
+        .service("DataService", DataService);
+}());
+
+// app.component
+(function() {
+    "use strict";
+
+    AppComponentController.$inject = ["$log", "DataService"];
+    function AppComponentController($log, dataService) {
+        this.$log = $log;
+        this.dataService = dataService;
+
+        this.$onInit = $onInit.bind(this);
+        this.loadData = loadData.bind(this);
+
+        function $onInit() {
+            this.$log.info("[AppComponent] onInit");
+            this.greetTarget = "World";
+        }
+
+        function loadData() {
+            this.$log.info("[AppComponent] loadData");
+            this.dataService
+                .getData()
+                .then(bindData.bind(this))
+                .catch(handleError.bind(this));
+        }
+
+        function bindData(newGreetTarget) {
+            this.$log.info("[AppComponent] bindData", newGreetTarget);
+            this.greetTarget = newGreetTarget;
+        }
+
+        function handleError(err) {
+            this.$log.info("[AppComponent] handleError", err);
+            alert(err);
+        }
+    }
+
+    var AppComponent = {
+        controller: AppComponentController,
+        template: [
+            "<div>",
+            "    Hello, {{$ctrl.greetTarget}}!<br>",
+            "    <button type=\"button\" ng-click=\"$ctrl.loadData()\">Load Data...</button>",
+            "</div>"
+        ].join("")
+    };
+
+    angular
+        .module("ajs.hello.world")
+        .component("appRoot", AppComponent);
+}());
+
+// main: bootstrapper
+(function() {
+    "use strict";
 
     angular.bootstrap(document.body, ["ajs.hello.world"]);
 }());
